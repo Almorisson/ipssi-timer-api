e2050c8f62e7ff7e993e48c78e7d196bccfcdd74
Mor NDIAYE <mo.ndiaye@ecole-ipssi.net>
2020-06-18 23:02:20 +0200

    [feat]: Finish up the CRUD of team model
    **Recap**:
    - Create/update/delete a team
    - Query all teams created by a user
    - Query a single team created by a user

--- resolvers/team.js
+++ resolvers/team.js
@@ -40,6 +40,51 @@ const createTeam = async (_, args, { req }) => {
 		console.log(error.message);
 	}
 };
+/**
+ * Allow to update a team when user is admin of this one
+ * @see authCheck method to better understand how we verified that user is current logged in user
+ *
+ * @param {*} _
+ * @param {*} args
+ * @param {*} param2
+ */
+const updateTeam = async (_, args, { req }) => {
+	const currentUser = await authCheck(req);
+	// validation
+	if (args.input.name.trim() === '' && args.input.description.trim() === '' && args.input.users.length === 0)
+		throw new Error('Vous devez renseigner au moins un des champs pour meettre à jour cette équipe');
+	//  get current user mongodb _id based in email
+	const currentUserFromDb = await User.findOne({ email: currentUser.email }).exec();
+	// _id of team to update
+	const teamToUpdate = await Team.findById({ _id: args.input._id }).exec();
+	// Allow update if current user id and id of the team's admin are same,
+	if (currentUserFromDb._id.toString() !== teamToUpdate.admin._id.toString())
+		throw new Error("Vous n'avez les droits nécessaires pour effectuer cette action.");
+	let updatedTeam = await Team.findByIdAndUpdate(args.input._id, { ...args.input }, { new: true })
+		.exec()
+		.then((team) => team.populate('admin', '_id username name').execPopulate());
+
+	return updatedTeam;
+};
+/**
+ * Allow to delete a team when user is admin of this one
+ * @see authCheck method to better understand how we verified that user is current logged in user
+ *
+ * @param {*} _
+ * @param {*} args
+ * @param {*} param2
+ */
+const deleteTeam = async (_, args, { req }) => {
+	const currentUser = await authCheck(req);
+	const currentUserFromDb = await User.findOne({ email: currentUser.email }).exec();
+	const teamToDelete = await Team.findById({ _id: args.teamId }).exec();
+	if (currentUserFromDb._id.toString() !== teamToDelete.admin._id.toString()) throw new Error('Unauthorized action');
+	let deletedTeam = await Team.findByIdAndDelete({ _id: args.teamId })
+		.exec()
+		.then((team) => team.populate('admin', '_id username name').execPopulate());
+
+	return deletedTeam;
+};
 
 /**
  * Get all teams created by a given user(the admin of this team)
@@ -65,15 +110,28 @@ const teamsCreatedByAdmin = async (_, args, { req }) => {
 };
 
 /**
+ * Get a single team infos
+ *
+ * @param {*} parent
+ * @param {*} args
+ */
+const singleTeam = async (_, args) => {
+	return await Team.findById({ _id: args.teamId }).populate('admin', '_id username name').exec();
+};
+
+/**
  * Exports all queries and mutations
  */
 module.exports = {
 	DateTime: DateTimeResolver,
 	Query: {
-		teamsCreatedByAdmin
+		teamsCreatedByAdmin,
+		singleTeam
 	},
 
 	Mutation: {
-		createTeam
+		createTeam,
+		updateTeam,
+		deleteTeam
 	}
 };
